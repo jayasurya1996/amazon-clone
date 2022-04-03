bdcc33ac1e921c6336dad37e2fd3d55b8207c1f9
jayasurya94 <jayasurya@twilightsoftwares.com>
2022-04-03 15:03:50 +0530

    home page changes

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBzoqfr3wcHLO26on5k2Ma6xw-yTbgDx2k",
+    authDomain: "clone-5845d.firebaseapp.com",
+    projectId: "clone-5845d",
+    storageBucket: "clone-5845d.appspot.com",
+    messagingSenderId: "349479016660",
+    appId: "1:349479016660:web:3217922783bbeb2d7bc341",
+    measurementId: "G-LT5RF7STSN"
+  };
